313953f98a246b07f597b13ef5b745bfcae60964
ihoover <polonian@gmail.com>
2015-03-30 04:59:16 -0400

    Qforms fully implemented, ClassGroup added

--- qform.py
+++ qform.py
@@ -1,4 +1,10 @@
 from algebra import *
+import math
+
+
+class Coeffs(object):
+    pass
+
 
 class Qform(Element):
     
@@ -11,7 +17,7 @@ class Qform(Element):
         
         if (abs(b) <= a) and (a <= c):
             if (c == a) or (abs(b) == a):
-                if b > 0:
+                if b >= 0:
                     return True
                 else:
                     return False
@@ -31,7 +37,7 @@ class Qform(Element):
         return (c,-b,a)
     
     @classmethod
-    def sheer(cls, a, b, c, m):
+    def sheer(cls, a, b, c, m=1):
         """
         x -> x+m*y
         y -> y
@@ -46,17 +52,21 @@ class Qform(Element):
         """
         
         while not cls.isReduced(a,b,c):
-            
             # find why it is failing
             if (c<a):
                 (a,b,c) = cls.rotate(a,b,c)
                 continue
             
             if (abs(b) > a):
-                m = (b - b%(2*a))/(2*a)
+                
+                if (2*a > b):
+                    m = -1
+                else:
+                    m = (b - b%(2*a))/(2*a)
+    
                 if sign(a*m) == sign(b):
                     m = -m
-                
+    
                 (a,b,c) = cls.sheer(a,b,c,m)
             
             if (b < 0) and (a == c):
@@ -68,11 +78,138 @@ class Qform(Element):
         
         return (a,b,c)
     
-    def __init__(a,b,c):
+    def __init__(self, a,b,c):
         """
         Create the equiv class of qudratic forms properly equiv to
             a*x^2 + b*xy+c*y^2
         a,b,c: int
         """
+
+        self.coeffs = Qform.reduced(a,b,c)
+        self.a = self.coeffs[0];
+        self.b = self.coeffs[1];
+        self.c = self.coeffs[2];
         
-        self.reduced_form = reduced(a,b,c)
+        self.D = self.b**2 - 4*self.a*self.c
+
+    def __mul__(self, other):
+        """
+        Multiply equivalence classes
+        """
+        
+        if self.D != other.D:
+            msg = "Discriminants don't match"
+            raise ValueError(msg)
+        
+        other_coeffs = other.coeffs
+        
+        counter = 0
+        while gcd (self.a, other_coeffs[0], (self.b + other_coeffs[1])/2) !=1:
+            
+            # un-reduce it until the relation is satisfied
+            if counter % 2 == 0:
+                other_coeffs = Qform.rotate(*other_coeffs)
+            else:
+                other_coeffs = Qform.sheer(*other_coeffs, m=(counter+1)/2)
+            counter +=1
+
+        # now solve the relation
+        # 1) B == b  mod 2a
+        # 2) B == b' mod 2a'
+        # 3) B^2 == D  mod 4aa'
+        #
+        # B is unique mod 2aa'
+        
+        # 2*a*a'
+        taap = 2*self.a*other_coeffs[0]
+
+        condition1 = set((2*self.a*x + self.b)%taap for x in range(taap))
+        condition2 = set((2*other_coeffs[0]*x + other_coeffs[1])%taap for x in range(taap))
+        condition3 = set(x for x in range(taap) if x**2 % (2*taap) == self.D% (2*taap))
+        
+        all_conditions = condition1 & condition2 & condition3
+        if len(all_conditions) != 1:
+            print condition1, condition2, condition3
+            print self, other
+            msg = str.format("No unique solution to congruence.  Solutions: {}", all_conditions)
+            raise ValueError(msg)
+        
+        B = all_conditions.pop()
+        
+        return Qform(taap/2, B, (B**2 - self.D)/(2*taap))
+    
+    def __hash__(self):
+        return self.coeffs.__hash__()
+    
+    def __eq__(self,other):
+        return self.coeffs == other.coeffs
+    
+    def __str__(self):
+        if self.b<0:
+            op1 = '-'
+        else:
+            op1 = '+'
+        if self.c<0:
+            op2 = '-'
+        else:
+            op2 = '+'
+        return str.format("{0}x^2 {1} {2}xy {3} {4}y^2", self.a, op1, abs(self.b), op2, abs(self.c))
+    
+    def __repr__(self):
+        return str(self.coeffs)
+    
+    def identity(self):
+        if (self.D % 4) == 1:
+            return Qform(1,1,(1-self.D)/4)
+        else:
+            return Qform(1,0,-self.D/4)
+    
+    def inv(self):
+        if (self.a == self.c) or (abs(self.b) == self.a):
+            return self
+        else:
+            return Qform(self.a, -self.b, self.c)
+
+
+class ClassGroup(Group):
+    """
+    The group of equivalence classes of quadratic forms of a give descriminent
+    The equivalence relation is proper equivalence
+    """
+    
+    @classmethod
+    def allReduced(cls, D):
+        def cValue(a,b,D):
+            """
+            calculate c value, return None of c isn't an integer
+            """
+            
+            c = (b**2-D)/(4.0*a)
+            
+            if int(c)==c:
+                return int(c)
+                
+            else:
+                return None
+        
+        # calculate reduced forms
+        forms = []
+        
+        # step one, calculate upper bound for a
+        max_a = int(math.floor(math.sqrt(-D/3.0)))
+        
+        # loop thru all possibilities
+        for a in range(1,max_a+1):
+            for b in range(0,a+1):
+                c = cValue(a,b,D)
+                if not(c is None) and gcd(a,gcd(b,c))==1 and c>=a:
+                    forms.append(Qform(a,b,c))
+                    if b<a and b!=0 and a!=c:
+                        forms.append(Qform(a,-b,c))
+        return forms
+    
+    def __init__(self, D):
+        
+        # these reduced forms are our elements
+        forms = self.allReduced(D)
+        super(ClassGroup, self).__init__(forms)
--- tests/test_qform.py
+++ tests/test_qform.py
@@ -1,5 +1,5 @@
 import unittest
-from qform import Qform
+from qform import Qform, ClassGroup
 
 class TestQformStatic(unittest.TestCase):
     """
@@ -15,6 +15,9 @@ class TestQformStatic(unittest.TestCase):
     def testIsReduced_false_bneg(self):
         self.assertFalse(Qform.isReduced(5,-3,5))
     
+    def testIsReduced_true_bzero(self):
+        self.assertTrue(Qform.isReduced(5,0,5))
+    
     def testIsReduced_true(self):
         self.assertTrue(Qform.isReduced(3,2,5))
     
@@ -25,4 +28,41 @@ class TestQformStatic(unittest.TestCase):
     def testReduced_already_reduced(self):
         (a,b,c) = (1,1,2)
         self.assertEqual(Qform.reduced(a,b,c), (a,b,c))
+
+
+class TestQform(unittest.TestCase):
     
+    def testInstantiate(self):
+        qform = Qform(1,2,3)
+    
+    def testStr_allPos(self):
+        q = Qform(1,0,1)
+        self.assertNotIn('-', str(q))
+    
+    def testStr_neg(self):
+        q = Qform(2,-1,3)
+        self.assertIn('- 1', str(q))
+    
+    def testMulAbelian(self):
+        q1 = Qform(4,4,17)
+        q2 = Qform(5,-2,13)
+        self.assertEqual(q1*q2, q2*q1)
+    
+    def testMul(self):
+        q1 = Qform(4,4,17)
+        q2 = Qform(5,-2,13)
+        self.assertEqual(q1*q2, Qform(20,28,13))
+    
+    def testIdentity(self):
+        for n in range(1,100):
+            for form in ClassGroup.allReduced(-4*n):
+                self.assertEqual(form*form.identity(), form)
+            for form in ClassGroup.allReduced(-4*n + 1):
+                self.assertEqual(form*form.identity(), form)
+    
+    def testInv(self):
+        for n in range(1,100):
+            for form in ClassGroup.allReduced(-4*n):
+                self.assertEqual(form*form.inv(), form.identity())
+            for form in ClassGroup.allReduced(-4*n + 1):
+                self.assertEqual(form*form.inv(), form.identity())
